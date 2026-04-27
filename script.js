41300ecda34d1b7a6a430ce273c1728e3e621469
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-04-27 12:20:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+<div class="col-auto">
+      <div class="summery">
+		  <h3>Summery</h3>
+		  
+		  <div>
+		  <p >sub total</p>
+			<p class>$360</p>
+		  <p>sub total</p>
+			<p>$360</p>
+		  <p>sub total</p>
+			<p>$360</p>
+		  <p>sub total</p>
+			<p>$360</p>
+		  </div>
+	  </div>
+    </div>
